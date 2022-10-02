14a55800aed9c70799bd23570dd6d92229d28462
JustinLien0805 <6998dragoan@gmail.com>
2022-10-02 14:29:19 +0800

    Initial Commit

---
+++ better-rest/better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_restApp.swift
+//  better-rest
+//
+//  Created by 練家辰 on 2022/10/2.
+//
+
+import SwiftUI
+
+@main
+struct better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
